3aeb1d4de379c341f3c3534bc70958f83ddb8d25
Pavan Gudipati <gpavankumar1974@gmail.com>
2018-05-31 15:25:31 -0700

    read json

--- ScriptFiles/readJson.R
+++ ScriptFiles/readJson.R
@@ -6,14 +6,16 @@
 library("rjson")
 
 # Give the input file name to the function.
-result <- fromJSON(file = "C:\\Users\\radhapavan\\Documents\\GitHub\\Rprogram\\Files\\readJson.json")
-
+#result <- fromJSON(file = "C:\\Users\\radhapavan\\Documents\\GitHub\\Rprogram\\Files\\readJson.json")
+result <- fromJSON(file = "C:\\Users\\radhapavan\\Documents\\GitHub\\Rprogram\\Files\\readJson2.json")
 # Print the result.
-print(result)
+#print(result)
 
-print(result$regionCode)
+#print(result$pageInfo)
+#print(result$items)
+print(result$items[1][1])
 
 # Convert JSON file to a data frame.
-json_data_frame <- as.data.frame(result)
+#json_data_frame <- as.data.frame(result)
 
-print(json_data_frame)
+#print(json_data_frame)
