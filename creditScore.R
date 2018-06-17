bc4eabbb1a1a513cd0c5ccffbd4bb229f47e6a69
Gudipati Pavan Kumar <gpavankumar1974@gmail.com>
2018-06-17 11:25:49 +0530

    credit

--- creditScore.R
+++ creditScore.R
@@ -15,6 +15,43 @@ print(str(yourdata.age))
 
 
 # New age column in added
-cbind(analysis.anova,yourdata.age)
+# New age column in added
+newdata <- cbind(analysis.anova,yourdata.age)
 print(analysis.anova)
+print(head(newdata))
+
+## count, monthly Income
+library(dplyr)
+result <- newdata %>% group_by(employmentType) %>%  summarize(n(), sum(monthlyIncome) , meansinc = mean(monthlyIncome))
+#barplot(newdata$monthlyIncome, names.arg=newdata$emplomentType)
+print(result)
+print(str(result))
+
+# bar 
+category1 <- c("1099/Self Employed","Fixed Income" , "W2 Employee" )
+colr <- c("red","green","blue")
+barplot(result$meansinc, names.arg=category1, col =colr)
+
+print(count(newdata, creditScore, employmentType))
+
+table1 <- table(result2$employmentType, dnn=c( "creditScore","employmentType" )) # Creates a contingency table
+addmargins(table1) #Displays the table (Not necessary)
+
+barplot(table1, ylab="Frequency", xlab="Gears", main="Side-By-Side Bar Chart", col=c("turquoise4", "turquoise2", "turquoise" ), beside=TRUE, width=.3)
+
+legend("right", title="Cylinders", legend= sort(unique(result2$employmentType)), fill =c("turquoise4", "turquoise2", "turquoise" ), box.lty=0)
+
+
+
+result2 <- newdata %>%   group_by(creditScore , employmentType) %>%    summarise(ct = n())
+print(result2)
+
+print(class(result2))
+
+table1 <- table(newdata$creditScore, newdata$employmentType, dnn=c("creditScore,", "employmentType")) # Creates a contingency table
+addmargins(table1) #Displays the table (Not necessary)
+
+barplot(table1, ylab="Total count", xlab="employmen tType", main="Side-By-Side Bar Chart", col=c("green", "blue", "orange" ), beside=TRUE, width=.3)
+
+legend("top", title=" Type of Employess Vs. Credit Scores", legend= sort(unique(result2$employmentType)), fill =c("green", "blue", "orange" ), box.lty=0)
 
